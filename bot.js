9950773a939eb15d85d12fd23471878f72bda60a
MertGokhanDonmez <mertgokhandonmez@gmail.com>
2025-10-14 14:17:51 +0300

    hourly statistic tests

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const rwClient = client.readWrite;
 const thresholds = {
   btcusdt: 3.0,
   ethusdt: 3.0,
-  xrpusdt: 5.0,
+  xrpusdt: 3.0,
 };
 
 const firstPrice = {};
@@ -29,7 +29,7 @@ const ws = new WebSocket("wss://stream.binance.com:9443/ws");
 ws.on("open", () => {
   const params = {
     method: "SUBSCRIBE",
-    params: ["btcusdt@ticker", "ethusdt@ticker", "xrpusdt@ticker"],
+    params: ["btcusdt@ticker_1h", "ethusdt@ticker_1h", "xrpusdt@ticker_1h"],
     id: 1,
   };
   ws.send(JSON.stringify(params));
@@ -46,46 +46,58 @@ ws.on("message", async (msg) => {
   const time = data.E; // event time
 
   // Record the first price and time for each symbol
-  if (!(symbol in firstPrice)) {
-    firstPrice[symbol] = price;
-    firstTimestamp = time;
-    alertState[symbol] = false;
-    console.log(
-      "Started tracking",
-      symbol,
-      "at:",
-      new Date(time),
-      "Price:",
-      price
-    );
-    return;
-  }
+  // if (!(symbol in firstPrice)) {
+  //   firstPrice[symbol] = price;
+  //   firstTimestamp = time;
+  //   alertState[symbol] = false;
+  //   console.log(
+  //     "Started tracking",
+  //     symbol,
+  //     "at:",
+  //     new Date(time).toString(),
+  //     "Price:",
+  //     price
+  //   );
+  //   return;
+  // }
 
   // if an hour has passed, reset the base price and time for this symbol
-  if (time - firstTimestamp >= 60 * 60 * 1000) {
-    firstPrice[symbol] = price;
-    firstTimestamp = time;
-    alertState[symbol] = false;
-  }
+  // if (time - firstTimestamp >= 60 * 60 * 1000) {
+  //   firstPrice[symbol] = price;
+  //   firstTimestamp = time;
+  //   alertState[symbol] = false;
+  //   console.log(new Date(time).toString());
+  // }
 
   // change percentage
-  const diff = ((price - firstPrice[symbol]) / firstPrice[symbol]) * 100;
+  // const diff = ((price - firstPrice[symbol]) / firstPrice[symbol]) * 100;
 
-  if (!alertState[symbol] && Math.abs(diff) >= thresholds[symbol]) {
+  // if (!alertState[symbol] && Math.abs(diff) >= thresholds[symbol]) {
+
+  const diff = data.P; // price change percent in the last 1 hour
+  const openTime = data.O; // ticker open time
+  const closeTime = data.C; // ticker close time
+  const highPrice = data.h; // high price in the last 1 hour
+  const lowPrice = data.l; // low price in the last 1 hour
+  const volume = data.v; // total traded base asset volume
+  const quoteVolume = data.q; // total traded quote asset volume
+
+  // if (openTime >)
+  if (Math.abs(diff) >= thresholds[symbol]) {
     const direction = diff > 0 ? "UP" : "DOWN";
     const alert = `${
       direction == "UP" ? "🚀" : "🔻"
-    } ${symbol.toUpperCase()} moved in an hour ${diff.toFixed(
+    } ${symbol.toUpperCase()} moved in an hour ${diff}% ${direction} (last: ${price.toFixed(
       2
-    )}% ${direction} (last: ${price.toFixed(2)} USDT)`;
+    )} USDT)`;
     console.log(alert);
 
-    try {
-      await rwClient.v2.tweet(alert);
-      console.log(alert);
-    } catch (err) {
-      console.error("Error posting tweet:", err);
-    }
+    // try {
+    //   await rwClient.v2.tweet(alert);
+    //   console.log(alert);
+    // } catch (err) {
+    //   console.error("Error posting tweet:", err);
+    // }
     alertState[symbol] = true; // set alert state to true to avoid repeated alerts for this symbol
   }
 });
